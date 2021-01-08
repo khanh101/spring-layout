244f365c7889587fb0dd5efe5890c0acf615d42f
khanhhhh <nguyenngockhanh.pbc@gmail.com>
2021-01-08 18:18:47 +0800

    first

---
+++ animation.py
@@ -0,0 +1,81 @@
+from typing import Iterator
+
+import numpy as np
+from matplotlib import pyplot as plt, animation
+from matplotlib.axes import Axes
+from matplotlib.collections import PathCollection
+from matplotlib.figure import Figure
+from matplotlib.lines import Line2D
+
+
+def draw(data_iter: Iterator[tuple[
+    np.ndarray,
+    list[list[int]],
+    tuple[tuple[int, int], tuple[int, int]],
+]], s: float=10, lw: float=1, c: str="C0"):
+    '''
+    data_iter: an iterator of data
+    example:
+        np.array([
+            [0, 1], # point 0
+            [2, 3], # point 1
+            [4, 5]  # point 2
+        ]),
+        [
+            [0, 1, 2], # path 1 2 3
+            [2, 0], # path 2 1
+        ],
+        (
+            (xmin, xmax),
+            (ymin, ymax),
+        ),
+    '''
+    fig: Figure
+    ax: Axes
+    fig, ax = plt.subplots()
+    ax.set_aspect("equal")
+    scat: PathCollection = ax.scatter([], [], s=s, c=c)
+    line_list: list[Line2D] = []
+
+    def draw(data: tuple[
+        np.ndarray,
+        list[list[int]],
+        tuple[tuple[int, int], tuple[int, int]],
+    ]):
+        coord, line_data_list, xylim = data
+        (xmin, xmax), (ymin, ymax) = xylim
+        ax.set_xlim(xmin, xmax)
+        ax.set_ylim(ymin, ymax)
+        scat.set_offsets(coord)
+        if len(line_data_list) > len(line_list):
+            for _ in range(len(line_data_list) - len(line_list)):
+                line_list.append(ax.plot([], [], lw=lw, c=c)[0])
+        for i, line_data in enumerate(line_data_list):
+            x = coord[line_data, 0]
+            y = coord[line_data, 1]
+            line_list[i].set_data(x, y)
+
+    ani = animation.FuncAnimation(
+        fig=fig,
+        func=draw,
+        frames=data_iter,
+        blit=False,
+        interval=10,
+        repeat=False,
+    )
+    plt.show()
+
+if __name__ == "__main__":
+    def data_iter() -> Iterator[tuple[
+        np.ndarray,
+        list[list[int]],
+        tuple[tuple[int, int], tuple[int, int]],
+    ]]:
+        point = np.random.normal(loc=0, scale=1, size=(100, 2))
+        path = [list(range(-1, 100))]
+        xylim = ((-3, +3), (-3, +3))
+        while True:
+            yield point, path, xylim
+            point += np.random.normal(loc=0, scale=0.01, size=point.shape)
+
+    draw(data_iter())
---
+++ main.py
@@ -0,0 +1,92 @@
+from typing import Callable, Any, Iterator, Union, Optional
+import numpy as np
+import scipy as sp
+import scipy.optimize
+import multiprocessing as mp
+from animation import draw
+
+class SpringLayout:
+    def __init__(self, adj: np.ndarray, d: int=2, coord: Optional[np.ndarray] = None):
+        self.adj = 0.5 * (adj + adj.T)
+        self.n = self.adj.shape[0]
+        self.d = d
+        for u in range(self.n):
+            self.adj[u, u] = 0
+        self.edge_list = []
+        for u in range(self.n):
+            for v in range(u+1, self.n):
+                if adj[u, v] > 0:
+                    self.edge_list.append([u, v])
+        if coord is None:
+            coord = np.random.normal(loc=0, scale=0.01, size=(self.n, self.d))
+        self.coord = coord
+
+    def data_iter(self) -> Iterator[tuple[
+        np.ndarray,
+        list[list[int]],
+        tuple[tuple[int, int], tuple[int, int]],
+    ]]:
+        parent_conn, child_conn = mp.Pipe()
+        process = mp.Process(target=SpringLayout._optimizer, args=(self, child_conn))
+        process.start()
+        while True:
+            coord = parent_conn.recv()
+            if coord is StopIteration:
+                break
+            yield coord, self.edge_list, ((coord[:, 0].min()-1, coord[:, 0].max()+1), (coord[:, 1].min()-1, coord[:, 1].max()+1))
+        process.join()
+
+    def _optimizer(self, child_conn: mp.Pipe):
+        x0 = self.coord.flatten()
+        def objective(x: np.ndarray) -> float:
+            coord = x.reshape((self.n, self.d))
+            obj = 0
+            for u in range(self.n):
+                for v in range(u + 1, self.n):
+                    dist = np.sqrt(((coord[u] - coord[v]) ** 2).sum())
+                    obj += self.adj[u, v] * dist ** 2 + dist ** (-1)
+            return obj
+
+        def callback(x: np.ndarray):
+            print(objective(x))
+            coord = x.reshape((self.n, self.d))
+            child_conn.send(coord)
+
+        result = sp.optimize.minimize(
+            fun=objective,
+            x0=x0,
+            method="L-BFGS-B",
+            callback=callback,
+        )
+        if result.success:
+            callback(result.x)
+        print("done", flush=True)
+        child_conn.send(StopIteration)
+
+np.random.seed(1234)
+
+height = 5
+width = 5
+n = height * width
+
+hw2i = np.arange(0, n).reshape((height, width))
+
+def within_range(h: int, w: int) -> bool:
+    if h < 0 or h >= height:
+        return False
+    if w < 0 or w >= width:
+        return False
+    return True
+
+adj = np.zeros(shape=(n, n), dtype=float)
+for h0 in range(height):
+    for w0 in range(width):
+        for dh, dw in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                h1 = h0+dh
+                w1 = w0+dw
+                if within_range(h1, w1):
+                    i0 = hw2i[h0, w0]
+                    i1 = hw2i[h1, w1]
+                    adj[i0, i1] = 1
+
+draw(SpringLayout(adj).data_iter(), s=20)
